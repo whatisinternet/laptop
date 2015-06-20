8162b9dc276d274b534679fdaeb25f08541de9f4
Whatisinternet <joshteeter@gmail.com>
2015-06-20 12:00:05 -0400

    Updated to work with raspberry-pi

--- base.sh
+++ base.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-
-sudo apt-get update
-sudo apt-get install -y -q ansible
+sudo apt-get install python-dev
+sudo apt-get install python-pip
+sudo apt-get install python3-pip
+sudo easy_install pip
+sudo pip install paramiko PyYAML Jinja2 httplib2
+sudo pip install ansible
 
 ansible-playbook -K  -i 'localhost ansible_connection=local,' laptop.yml
